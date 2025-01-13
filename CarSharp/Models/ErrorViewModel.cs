bf61fbe2b4cc7115638482299bbb75d2626234cd
xideme <soopka@gmail.com>
2025-01-13 14:32:40 +0200

    First commit

---
+++ CarSharp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarSharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
